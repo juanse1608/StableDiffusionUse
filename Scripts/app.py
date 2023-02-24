e4dfd336ab453ed217d33d60950f5f6a7c004ec4
juanse1608 <js.uniandes10@gmail.com>
2023-02-24 09:56:18 -0500

    Adjusting parameters

--- Scripts/app.py
+++ Scripts/app.py
@@ -31,7 +31,7 @@ except:
 stability_api = client.StabilityInference(
     key=stability_key, # Api key reference.
     verbose=True, # Print debug messages.
-    engine="stable-diffusion-v1-5", # Set the engine to use for generation. 
+    engine="stable-diffusion-v2-0", # Set the engine to use for generation. 
     # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0 
     # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
 )
