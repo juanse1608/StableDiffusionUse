e8d41eb6e8e202c120caa20241f73f615748c17f
juanse1608 <js.uniandes10@gmail.com>
2023-02-24 09:06:43 -0500

    Adding parameters and more

--- Scripts/app.py
+++ Scripts/app.py
@@ -11,10 +11,12 @@ import json
 import warnings
 import logging
 from password import check_password
+import itertools
 
 
 # Configuration
 width = 700
+height = 400
 os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
 try:
     with open('Tokens/tokens.json', 'r') as file:
@@ -58,45 +60,57 @@ if (('LOCAL_MACHINE' in st.session_state.keys()) and (st.session_state['LOCAL_MA
         background_image=Image.open(bg_image) if bg_image else None,
         update_streamlit=realtime_update,
         drawing_mode=drawing_mode,
-        width=width,
+        width=width, 
+        height=height,
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         key="canvas",
         display_toolbar=True
     )
-
-    # Set up our initial generation parameters.
+    
+    schedules = np.arange(0.1,1.1,0.1)
+    scales = np.arange(1,11,1)
     prompt = st.text_input("")
     button = st.button(label='Generate Image')
+    imgs = []
     if button:
-        answers = stability_api.generate(
-            # If you have an init image
-            init_image=Image.fromarray(canvas_result.image_data).resize((256,256)),
-            prompt=prompt,
-            start_schedule=0.5,
-            seed=992446758, # If a seed is provided, the resulting generated image will be deterministic.
-                            # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
-                            # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
-            steps=30, # Amount of inference steps performed on image generation. Defaults to 30. 
-            cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
-                        # Setting this value higher increases the strength in which it tries to match your prompt.
-                        # Defaults to 7.0 if not specified.
-            width=512, # Generation width, defaults to 512 if not included.
-            height=512, # Generation height, defaults to 512 if not included.
-            samples=1, # Number of images to generate, defaults to 1 if not included.
-            sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
-                                                        # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
-                                                        # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
-        )
+        for schedule, scale in itertools.product(schedules, scales):
+            # Set up our initial generation parameters.
+            answers = stability_api.generate(
+                # If you have an init image
+                init_image=Image.fromarray(canvas_result.image_data).resize((256,256)),
+                prompt=prompt,
+                start_schedule=schedule,
+                seed=992446758, # If a seed is provided, the resulting generated image will be deterministic.
+                                # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                                # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+                steps=30, # Amount of inference steps performed on image generation. Defaults to 30. 
+                cfg_scale=scale, # Influences how strongly your generation is guided to match your prompt.
+                            # Setting this value higher increases the strength in which it tries to match your prompt.
+                            # Defaults to 7.0 if not specified.
+                width=width, # Generation width, defaults to 512 if not included.
+                height=height, # Generation height, defaults to 512 if not included.
+                samples=1, # Number of images to generate, defaults to 1 if not included.
+                sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
+                                                            # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
+                                                            # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
+            )
 
-        # Set up our warning to print to the console if the adult content classifier is tripped.
-        # If adult content classifier is not tripped, save generated images.
-        for resp in answers:
-            for artifact in resp.artifacts:
-                if artifact.finish_reason == generation.FILTER:
-                    warnings.warn(
-                        "Your request activated the API's safety filters and could not be processed."
-                        "Please modify the prompt and try again.")
-                if artifact.type == generation.ARTIFACT_IMAGE:
-                    img = Image.open(io.BytesIO(artifact.binary))
-                st.image(img, width=width)
-                    # img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
+            # Set up our warning to print to the console if the adult content classifier is tripped.
+            # If adult content classifier is not tripped, save generated images.
+            for resp in answers:
+                for artifact in resp.artifacts:
+                    if artifact.finish_reason == generation.FILTER:
+                        warnings.warn(
+                            "Your request activated the API's safety filters and could not be processed."
+                            "Please modify the prompt and try again.")
+                    if artifact.type == generation.ARTIFACT_IMAGE:
+                        img = Image.open(io.BytesIO(artifact.binary))
+                        imgs.append(img)
+                        # st.image(img, width=width)
+                        # img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
+            
+            for idx_r in range(len(schedules)):
+                cols = st.columns(len(scales))
+                for idx_c, col in enumerate(cols):
+                    col.image(imgs[(idx_r*len(cols))+idx_c], width=width//10, height=height//10)
+                        
